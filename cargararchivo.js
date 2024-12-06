40b381dd21d9fd53ea25ecde2208241f7e874ca4
JCASALLAS <jcasallasnarvaez@gmail.com>
2024-12-06 16:53:18 -0500

    SE CREA OPC TEMPORAL PARA PUBLICAR RECURSOS

---
+++ cargararchivo.js
@@ -0,0 +1,27 @@
+const imageInput = document.getElementById('imageInput');
+        const carouselContent = document.getElementById('carouselExampleDark');
+        const toastElement = document.getElementById('successToast');
+        // Inicializar el toast de Bootstrap
+        const toast = bootstrap.Toast.getOrCreateInstance(toastElement);
+
+        imageInput.addEventListener('change', (event) => {
+            const files = event.target.files;
+            carouselContent.innerHTML = ''; // Limpiar carrusel
+
+            Array.from(files).forEach((file, index) => {
+                const reader = new FileReader();
+
+                reader.onload = (e) => {
+                    // Crear un contenedor para la imagen
+                    const carouselItem = document.createElement('div');
+                    carouselItem.className = `carousel-item ${index === 0 ? 'active' : ''}`;
+                    carouselItem.innerHTML = `
+                        <img src="${e.target.result}" class="d-block w-100" alt="Imagen ${index + 1}">
+                    `;
+                    carouselContent.appendChild(carouselItem);
+                };
+
+                reader.readAsDataURL(file); // Leer archivo como base64
+                toast.show();
+            });
+        });
